8c5c76268510b2bf91d2f6fcb381f6637ef2f46c
Sam Williams <samwilliams281@gmail.com>
2022-03-23 18:37:15 -0400

    add questions

--- index.js
+++ index.js
@@ -1,7 +1,103 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require("fs");
+const { prompt } = require("inquirer");
+const genMD = require("./utils/generateMarkdown");
+
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?(Required)",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter your projects name!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a description of your project(Required)",
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log("Please enter project description!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "Please provide installation instructions for your project(Required)",
+    validate: (installInput) => {
+      if (installInput) {
+        return true;
+      } else {
+        console.log("Please enter project installation instructions!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide usage instructions for your project(Required)",
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log("Please enter project usage instructions!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please select the license(s) used. If none please select 'None'(Required)",
+    choices: ["MIT", "Mozilla", "Apache", "GNU", "BSD", "None"],
+    validate: (licenseInput) => {
+      if (licenseInput) {
+        return true;
+      } else {
+        console.log("Please enter the projects license or select 'None'!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Who has contributed to this project?(Required)",
+    validate: (contributingInput) => {
+      if (contributingInput) {
+        return true;
+      } else {
+        console.log("Please enter project contributer(s)!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please provide instructions on how to test your project(Required)",
+    validate: (testInput) => {
+      if (testInput) {
+        return true;
+      } else {
+        console.log("Please enter project testing instructions!");
+        return false;
+      }
+    },
+  },
+];
+
+prompt(questions).then((ans) => console.log(genMD(ans)));
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // console.log(license == "None")
+  if (license == "None") {
+    return "";
+  }
+  return `![Badge](https://img.shields.io/badge/licence-${license}-green)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == "None") {
+    return "";
+  }
+  return `* [License](#license)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license == "None") {
+    return "";
+  }
+  return `
+  ## License 
+  This project is licensed under ${license}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# <center>${data.title}<center>
+  ===============================================
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  * [Contributing](#contributing)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.install}
+
+  ## Usage 
+  ${data.usage}
+
+  ## Contributions
+  ${data.contributing}
+
+  ${renderLicenseSection(data.license)} 
+  ${renderLicenseBadge(data.license[0])}
+
+  ## Testing
+  ${data.tests}
 `;
 }
 
